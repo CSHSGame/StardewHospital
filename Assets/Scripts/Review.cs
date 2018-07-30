86651648c30880d30f7f775fc4fc331fc1df02bb
DESKTOP-U3AI2IT\Julian <juliandisterheft@gmail.com>
2018-07-30 17:09:39 -0400

    yousaf and martin varibles linked. not for stein

--- Assets/Scripts/Review.cs
+++ Assets/Scripts/Review.cs
@@ -52,24 +52,24 @@ public class Review : MonoBehaviour {
 	void OnEnable () {
 //Mr. Martin
 	//Did you knock?
-		if (martinKnockedBool == true) {
+		if (variableStorage.GetValue("$open_door").AsBool == true) {
 			martinKnocked = "     Mr. Martin appreciated that you knocked before entering his room - it's important to remember that this is his home, and that we respect his privacy. ";
 		}else{
 			martinKnocked = "     Mr. Martin said you barged into his room without knocking - that's not acceptable. It's important to remember that this is his home, and that we respect the privacy of all our clients. ";
 		} // if knocked bool is false end the assessment
 
 	//Rude to mr. martin
-		if (martinRudeBool == true) {
+		if (variableStorage.GetValue("$rude_to_Martin").AsBool == true) {
 			martinRude = "\n     He didn't much care for the flippant tone with which you spoke to him. He's a very conservative person and you need to address him in a direct, professional manner. ";
 		}else{
 			martinRude = "\n     He said you spoke to him in a very polite and proffessional manner. He really appreciated that. ";
 		}
 	//Making plans with mr. martin
-		if (martinCheckedWithSupervisorBool == true)
+		if (variableStorage.GetValue("$1will_confirm_Martin").AsBool == true)
 		{
 			martinCheckedWithSupervisor = "\n     It's great that Mr. Martin offered to show you around! You should have some time free around 2pm tomorrow to take him up on his offer. ";
 		}else{
-			if (martinVagueBool == true)
+			if (variableStorage.GetValue("$vague_Martin").AsBool == true)
 			{
 				martinCheckedWithSupervisor = "\n     It's great that Mr. Martin offered to show you around, but he really responds better to making definite plans at specific times. In this case, you should have said you would check in with me for an appropriate time and then let him know. ";
 			} else {
@@ -81,43 +81,120 @@ public class Review : MonoBehaviour {
 //Mr.Youssaf
 	
 		//Mr Youssaf Leaves
-		if (youssafLeavesBool == true) 
+		if (variableStorage.GetValue("$yousaf_left").AsBool == true) 
 		{
 			youssafText.text = "     You allowed Mr. Youssaf to just leave?? Have you lost your mind?? Lucky for you security caught up with him at the gate, otherwise we'd be looking at a major lawsuit. Unbelievable... ";
 		}
 
 		//Rude to Mr Youssaf
-		if(youssafRudeBool == true && youssafLeavesBool == false)
+		if(variableStorage.GetValue("$rude_to_yousaf").AsBool == true && variableStorage.GetValue("$yousaf_left").AsBool == false)
 		{
 			youssafText.text = "     You were quite rude with Mr. Youssaf, especially considering you were told not be confrontational with him. You could have done better to diffuse that situation. ";
 		}
 
-		if(youssafRudeBool == false && youssafLeavesBool == false)
+		if(variableStorage.GetValue("$rude_to_yousaf").AsBool == false && variableStorage.GetValue("$yousaf_left").AsBool == false)
 		{
 			youssafText.text = "     I'm so impressed by the way you handled that situation with Mr. Youssaf - you made sure not to come off as confrontational and as a result diffused a situation that could have quickly escalated. Great work! ";
 		}
 
 // Mrs Stein
 		//Mrs Stein Fail
-		if (steinTooSoonBool == true)
+		if (variableStorage.GetValue("$SteinTooSoon").AsBool == true)
 		{
 			steinText.text = "     Mrs. Stein was quite upset with you today. You were just supposed to stop by and introduce yourself - what in the world made you hink it would be appropriate to bring up her health? Especially after being informed that it was a sensitive topic. We expect a little more thoughtfulness and empathy from you moving forward. ";
 		}
 		//Mrs Stein too pushy		
-		if (steinTooPushyBool == true)
+		if (variableStorage.GetValue("$steintoopushybool").AsBool == true)
 		{
 			steinText.text = "     Mrs Stein enjoyed meeting you, but you'll need to show a little more discretion in the future. Revealing that the secretary had informed you of her illness before you'd even met made her feel a little uncomfortable.";
 		}
 		//Mrs Stein Success
-		if (steinGoodBool == true)
+		if (variableStorage.GetValue("$Steingoodbool").AsBool == true)
 		{
 			steinText.text = "     You made a great first impression on Mrs Stein and did a great job of being empathetic without pushing or prying too much. ";
 		}
 	}
 
-	
-	
-	void Update () {
+    /*void OnEnable()
+    {
+        //Mr. Martin
+        //Did you knock?
+        if (martinKnockedBool == true)
+        {
+            martinKnocked = "     Mr. Martin appreciated that you knocked before entering his room - it's important to remember that this is his home, and that we respect his privacy. ";
+        }
+        else
+        {
+            martinKnocked = "     Mr. Martin said you barged into his room without knocking - that's not acceptable. It's important to remember that this is his home, and that we respect the privacy of all our clients. ";
+        } // if knocked bool is false end the assessment
+
+        //Rude to mr. martin
+        if (martinRudeBool == true)
+        {
+            martinRude = "\n     He didn't much care for the flippant tone with which you spoke to him. He's a very conservative person and you need to address him in a direct, professional manner. ";
+        }
+        else
+        {
+            martinRude = "\n     He said you spoke to him in a very polite and proffessional manner. He really appreciated that. ";
+        }
+        //Making plans with mr. martin
+        if (martinCheckedWithSupervisorBool == true)
+        {
+            martinCheckedWithSupervisor = "\n     It's great that Mr. Martin offered to show you around! You should have some time free around 2pm tomorrow to take him up on his offer. ";
+        }
+        else
+        {
+            if (martinVagueBool == true)
+            {
+                martinCheckedWithSupervisor = "\n     It's great that Mr. Martin offered to show you around, but he really responds better to making definite plans at specific times. In this case, you should have said you would check in with me for an appropriate time and then let him know. ";
+            }
+            else
+            {
+                martinCheckedWithSupervisor = "\n     It's great that Mr. Martin offered to show you around, but I had other plans for you tomorrow at that time! Next time please check in with me first.";
+            }
+        }
+        martinText.text = martinKnocked + martinRude + martinCheckedWithSupervisor;
+
+        //Mr.Youssaf
+
+        //Mr Youssaf Leaves
+        if (youssafLeavesBool == true)
+        {
+            youssafText.text = "     You allowed Mr. Youssaf to just leave?? Have you lost your mind?? Lucky for you security caught up with him at the gate, otherwise we'd be looking at a major lawsuit. Unbelievable... ";
+        }
+
+        //Rude to Mr Youssaf
+        if (youssafRudeBool == true && youssafLeavesBool == false)
+        {
+            youssafText.text = "     You were quite rude with Mr. Youssaf, especially considering you were told not be confrontational with him. You could have done better to diffuse that situation. ";
+        }
+
+        if (youssafRudeBool == false && youssafLeavesBool == false)
+        {
+            youssafText.text = "     I'm so impressed by the way you handled that situation with Mr. Youssaf - you made sure not to come off as confrontational and as a result diffused a situation that could have quickly escalated. Great work! ";
+        }
+
+        // Mrs Stein
+        //Mrs Stein Fail
+        if (steinTooSoonBool == true)
+        {
+            steinText.text = "     Mrs. Stein was quite upset with you today. You were just supposed to stop by and introduce yourself - what in the world made you hink it would be appropriate to bring up her health? Especially after being informed that it was a sensitive topic. We expect a little more thoughtfulness and empathy from you moving forward. ";
+        }
+        //Mrs Stein too pushy		
+        if (steinTooPushyBool == true)
+        {
+            steinText.text = "     Mrs Stein enjoyed meeting you, but you'll need to show a little more discretion in the future. Revealing that the secretary had informed you of her illness before you'd even met made her feel a little uncomfortable.";
+        }
+        //Mrs Stein Success
+        if (steinGoodBool == true)
+        {
+            steinText.text = "     You made a great first impression on Mrs Stein and did a great job of being empathetic without pushing or prying too much. ";
+        }
+    }
+
+    */
+
+    void Update () {
 		if (pageNumber == 1){
 			martinText.gameObject.SetActive(true);
 			youssafText.gameObject.SetActive(false);
