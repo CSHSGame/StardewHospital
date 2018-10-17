3954ef387cd5bfad2a9d512813c96efd1b49c772
JulianDesignDesktop <juliandisterheft@gmail.com>
2018-10-16 22:43:49 -0400

    Very Basic Waypointing. Flesh out later.

---
+++ Assets/Waypoints.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public GameObject[] points;
+    public GameObject character;
+    public float speed;
+
+    private int index = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        if (index < points.Length)
+        {
+            // Move our position a step closer to the target.
+            character.transform.position = Vector3.MoveTowards(character.transform.position, points[index].transform.position, step);
+
+            //If we've reached the destination, move to the next one
+            if (character.transform.position == points[index].transform.position)
+            {
+                index++;
+            }
+        }
+    }
+}
