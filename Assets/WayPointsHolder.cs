acee126c675599e8baa924c258eb71e1192139c5
JulianDesignDesktop <juliandisterheft@gmail.com>
2018-10-18 13:26:42 -0400

    MovingCharacter

---
+++ Assets/WayPointsHolder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class WayPointsHolder  {
+
+    public string Name;
+    public GameObject[] points;
+    [Space]
+    private bool test;
+}
--- Assets/Waypoints.cs
+++ Assets/Waypoints.cs
@@ -1,37 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Yarn.Unity;
+[SelectionBase]
 public class Waypoints : MonoBehaviour {
 
-    public GameObject[] points;
-    public GameObject character;
+    public WayPointsHolder[] holder;
+    
+
     public float speed;
+    private float step;
+    private int pathindex = -1;
+    private int nodeindex = 0;
 
-    private int index = 0;
 
-	// Use this for initialization
-	void Start ()
+    [YarnCommand("SetPath")]
+    public void StartPathing(string Pathnum)
+    {
+        Debug.Log("called " + Pathnum);
+        int returnValue = ConvertStringToInt(Pathnum);
+        pathindex = returnValue;
+    }
+
+    // Use this for initialization
+    void Start ()
     {
         
+    }
+
+    int ConvertStringToInt(string number)
+    {
+        switch (number)
+        {
+            case "Zero": return 0;
+            case "One": return 1;
+            case "Two": return 2;
+            case "Three": return 3;
+            case "Four": return 4;
+            case "Five": return 5;
+            case "Six": return 6;
+            case "Seven": return 7;
+            case "Eight": return 8;
+            case "Nine": return 9;
+            case "Ten": return 10;
+
+            default:
+                Debug.Break();
+                return -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // The step size is equal to speed times frame time.
-        float step = speed * Time.deltaTime;
-
-        if (index < points.Length)
+        step = speed * Time.deltaTime;
+        if (pathindex != -1)
         {
-            // Move our position a step closer to the target.
-            character.transform.position = Vector3.MoveTowards(character.transform.position, points[index].transform.position, step);
-
-            //If we've reached the destination, move to the next one
-            if (character.transform.position == points[index].transform.position)
+            if (nodeindex < holder[pathindex].points.Length)
             {
-                index++;
+                //Move our position a step closer to the target.
+                this.transform.position = Vector3.MoveTowards(this.transform.position, holder[pathindex].points[nodeindex].transform.position, step);
+
+                //If we've reached the destination, move to the next one
+                if (this.transform.position == holder[pathindex].points[nodeindex].transform.position)
+                {
+                    nodeindex++;
+                }
+            }
+            else
+            {
+                Debug.Log("Break");
+                pathindex = -1;
             }
         }
     }
+
+    /*void Day3WilliamsAndPlayerToRoom()
+    {
+        if (index < points.Length)
+        {
+
+        Williams.transform.position = Vector3.MoveTowards(Williams.transform.position, .transform.position, step);
+        if (character.transform.position == points[index].transform.position)
+        {
+                index++;
+        }
+        }
+    }*/
 }
