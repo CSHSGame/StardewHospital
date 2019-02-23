3f31e06bf71628736fbde8b988d94722cea0e4e8
JulianDesignDesktop <juliandisterheft@gmail.com>
2019-02-22 22:33:29 -0500

    HUD and Clipboard Stuff

---
+++ Assets/HUDController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour {
+
+    public Text RoomText;
+    public Text DayText;
+    public Text DayNumText;
+    public Text MonthText;
+    public Text TimeText;
+    public Text AMorPMText;
+
+    public GameObject ClipboardUI;
+    public DayDataHolder dayData;
+
+	// Use this for initialization
+	public void Setup (DayDataHolder day)
+    {
+        dayData = day;
+        DayChange();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void GearClick()
+    {
+       
+    }
+
+    public void ClipboardClick()
+    {
+        if (ClipboardUI.activeInHierarchy == true)
+        {
+            ClipboardUI.SetActive(false);
+        }
+        else if (ClipboardUI.activeInHierarchy == false)
+        {
+            ClipboardUI.SetActive(true);
+        }
+    }
+
+    public void  DayChange()
+    {
+        DayText.text = dayData.DayOfTheWeek;
+        DayNumText.text = dayData.DayNumber;
+        MonthText.text = dayData.Month;
+        TimeText.text = dayData.TimeOfDay;
+        if(dayData.AMOrPM == false)
+        {
+            AMorPMText.text = "AM";
+        }
+        else
+        {
+            AMorPMText.text = "PM";
+        }
+
+    }
+
+    
+}
--- Assets/Scripts/DayDataHolder.cs
+++ Assets/Scripts/DayDataHolder.cs
@@ -11,4 +11,9 @@ public class DayDataHolder : ScriptableObject
     public ObjectiveData[] Objectives;
 
     public NpcDayData[] npcs;
+    public string DayOfTheWeek; // 3 Character
+    public string DayNumber; // 2 Digits
+    public string Month; // 3-4 Characters
+    public string TimeOfDay; // 12:00 Time Format
+    public bool AMOrPM; // AM false, PM true
 }
